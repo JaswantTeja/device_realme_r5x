733905679b16422997e7941dbc2ece389af4b1e1
JaswantTeja <sunnyteja201@gmail.com>
2022-07-17 11:13:56 +0530

    r5x : Gapps Blaze

--- blaze_r5x.mk
+++ blaze_r5x.mk
@@ -16,6 +16,7 @@ $(call inherit-product, vendor/blaze/config/common_full_phone.mk)
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 TARGET_BOOT_ANIMATION_RES := 720
+WITH_GAPPS := true
 #TARGET_GAPPS_ARCH := arm64
 #TARGET_INCLUDE_LIVE_WALLPAPERS := false
 
