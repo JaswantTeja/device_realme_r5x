Sanskar-Chaudhary/device_realme_r5xx

r5x: sepolicy: Attach proper property_type into device_id * as per https://source.android.com/devices/architecture/configuration/add-system-properties.

BoardConfig.mk
@@ -202,6 +202,7 @@ VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 
 # Treble
 PRODUCT_VENDOR_MOVE_ENABLED := true

JaswantTeja/device_realme_r5x

BoardConfig.mk
@@ -203,6 +203,7 @@ SELINUX_IGNORE_NEVERALLOWS := true
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 
 # Treble
 PRODUCT_VENDOR_MOVE_ENABLED := true