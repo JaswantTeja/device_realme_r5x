090b5c22676e0addccd26360a48fba39da2519ad
JaswantTeja <sunnyteja201@gmail.com>
2022-12-12 16:01:10 +0000

    r5x: Init CipherOS
    Signed-off-by: JaswantTeja <sunnyteja201@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_r5x.mk
+    $(LOCAL_DIR)/cipher_r5x.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_r5x-user \
-    aosp_r5x-userdebug \
-    aosp_r5x-eng
+    cipher_r5x-user \
+    cipher_r5x-userdebug \
+    cipher_r5x-eng
--- cipher_r5x.mk
+++ cipher_r5x.mk
@@ -9,20 +9,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common AospExtended stuff
-$(call inherit-product, vendor/aosp/common.mk)
+# Inherit some common CipherOS stuff
+$(call inherit-product, vendor/cipher/config/common_full_phone.mk)
 
 # Inherit from r5x device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 TARGET_BOOT_ANIMATION_RES := 720
-#TARGET_GAPPS_ARCH := arm64
-#TARGET_INCLUDE_LIVE_WALLPAPERS := false
 
 PRODUCT_BRAND := Realme
 PRODUCT_DEVICE := r5x
 PRODUCT_MANUFACTURER := Realme
-PRODUCT_NAME := aosp_r5x
+PRODUCT_NAME := cipher_r5x
 PRODUCT_MODEL := Realme 5 Series
 
 PRODUCT_GMS_CLIENTID_BASE := android-realme
